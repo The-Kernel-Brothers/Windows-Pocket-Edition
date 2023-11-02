9fe4953ddb5e4c7df173e390e09a6d42c6532ba6
Alphe <140343441+alphe-lbp@users.noreply.github.com>
2023-11-02 19:37:15 +0400

    Add files via upload
    os_wpe is for windows pe
    line is for its own app/exe

---
+++ os_wpe.bat
@@ -0,0 +1,156 @@
+@echo off
+echo ....Welcome to Windows Pocket Edition....
+pause
+cls
+
+:start
+echo ....Start List....
+echo 1) Shutdown
+echo 2) Programs list
+echo 3) Tools
+echo 4) Control Panel
+echo 5) About WPE
+set /p soption=Select an option from the Start List: 
+if %soption%==1 call :sdown
+if %soption%==2 call :plistcls
+if %soption%==3 call :toolscls
+if %soption%==4 call :cpanelcls
+if %soption%==5 call :aboutwpe
+echo.
+goto start
+
+:sdown
+exit
+
+:plistcls
+cls
+goto plist
+
+:toolscls
+cls
+goto tools
+
+:cpanelcls
+cls
+goto cpanel
+
+:tools
+echo ....Tools....
+echo 1) Ping a website
+echo 2) Ping google.com
+echo 3) DiskPart
+echo 4) Go Back
+set /p toption=Select a tool: 
+if %toption%==1 call :pingsite
+if %toption%==2 call :pingg
+if %toption%==3 call :diskp
+if %toption%==4 goto rstart
+goto tools
+
+:pingsite
+echo.
+set /p toption=Enter the website you want to ping: 
+ping %toption%
+pause
+cls
+goto tools
+
+:pingg
+echo.
+ping google.com
+pause
+cls
+goto tools
+
+:diskp
+cls
+diskpart
+cls
+goto tools
+
+:cpanel
+echo ....Control Panel....
+echo Adjust your computer's settings.
+echo.
+echo 1) Change terminal colors
+echo 2) Exit Control Panel
+set /p coption=Choose a setting to change: 
+if %coption%==1 call :scolor
+if %coption%==2 goto rstart
+goto cpanel
+
+:scolor
+cls
+echo Color Change
+echo.
+echo 0 = Black        8 = Gray
+echo 1 = Blue         9 = Light Blue
+echo 2 = Green        A = Light Green
+echo 3 = Aqua         B = Light Aqua
+echo 4 = Red          C = Light Red
+echo 5 = Purple       D = Light Purple
+echo 6 = Yellow       E = Light Yellow
+echo 7 = White        F = Bright White
+echo.
+set /p bcoption=Choose the Background color: 
+echo.
+set /p fgoption=Choose the Text color: 
+color %bcoption%%fgoption%
+cls
+goto cpanel
+
+:rstart
+cls
+goto start
+
+:plist
+echo 1) Notepad
+echo 2) Task Manager
+echo 3) Paint.Net
+echo 4) Microsoft Paint 95
+echo 5) Return to Start List
+set /p poption=Select a program from the Programs List: 
+if %poption%==1 call :notepadappcls
+if %poption%==5 goto rstart
+if %poption%==2 call :taskmgrcls
+if %poption%==3 call :paintnet
+if %poption%==4 call :mspaint
+goto plist
+exit /b
+
+:paintnet
+cls
+cd paint
+paint.exe
+cd..
+goto plist
+
+:mspaint
+cls
+cd paint
+mspaint.exe
+cd..
+goto plist
+
+:notepadappcls
+cls
+goto notepadapp
+
+:notepadapp
+notepad.exe
+goto plist
+
+:taskmgrcls
+cls
+goto taskmgr
+
+:taskmgr
+taskmgr
+goto plist
+
+:aboutwpe
+cls
+echo Windows Pocket Edition - Version Alpha 0.0.2 (Build: A4) - Made by The Kernel Brothers
+pause
+cls
+goto start
